dcfa96fe3471e8b38e0044a077ec82affacfac0c
ikihiutale <tuokkolk@gmail.com>
2016-03-20 12:48:27 +0200

    db fix 12

--- helpers/sidebar.js
+++ helpers/sidebar.js
@@ -5,14 +5,18 @@ var stats = require('./stats'),
 
 // Called from crontroller/image
 module.exports = function(viewModel, cb) { 
+	console.log("sidebar...1");
 	async.parallel([
 	  function(next) {
+		  console.log("sidebar...2");
 		  stats(next);
 	  }, 
 	  function(next) { 
+		  console.log("sidebar...3");
 		  Images.popular(next); 
 	  }, 
 	  function(next) { 
+		  console.log("sidebar...4");
 		  Comments.newest(next);
 	  }],
 	  // The last parameter to the parallel function is an inline 
@@ -20,6 +24,7 @@ module.exports = function(viewModel, cb) {
 	  // This array is a collection of each of the results that were returned 
 	  // from each of the functions in the array in the first parameter. 
 	  function(err, results){ 
+		console.log("sidebar...5");
 		viewModel.sidebar = { 
 			stats: results[0], 
 			popular: results[1], 
