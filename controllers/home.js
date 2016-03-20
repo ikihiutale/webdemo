0384b55443223ba045f2fe12f88e06977eaeae0b
ikihiutale <tuokkolk@gmail.com>
2016-03-20 12:43:45 +0200

    db fix 11

--- controllers/home.js
+++ controllers/home.js
@@ -11,10 +11,14 @@ module.exports = {
 		// sidebar has completed populating viewModel -  this is because 
 		// of the asynchronous nature of Node.js
 		sidebar(viewModel, function(viewModel) {
+			console.log("Miksi ei toimi 2");
 			ImageModel.find({}, {}, { sort: { timestamp: -1 }}, function(err, images) { 
+				console.log("Miksi ei toimi 3");
 				if (err) { throw err; }
+				console.log("Miksi ei toimi 4");
 				viewModel.images = images; 
 				sidebar(viewModel, function(viewModel) { 
+					console.log("Miksi ei toimi 5");
 					res.render('index', viewModel);
 				});
 			}); 
